769338c7fd7e0726ce90da80c6bee3cbe2cce22c
Amiran <amiranlevanovich@gmail.com>
2021-03-09 23:29:42 +0300

    add complete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,14 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def complete
+    task = Task.find(params[:id])
+    task.complete!
+    task.save
+
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
